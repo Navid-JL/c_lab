de1313a1f9eb3ba33d610fd16b971346192f2794
Navid <navidjalili3@gmail.com>
2023-03-19 11:06:40 +0330

    Add app.c

---
+++ app.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float profit = 23.34f;
+    /*****************************
+     * Name: app.c               *
+     * Purpose: prints a bad pun *
+     * Author: Navid             *
+     * ***************************
+     */
+    int myNum = (int)profit;
+    printf("%d", myNum);
+    // printf("To C, or not to C, that is the question.\n");
+    return 0;
+}
